b2bee2e2d762e7c17e719b870751730fff1dcb92
Nicolas Raigosa <nraigosam@eafit.edu.co>
2021-03-23 23:06:00 -0500

    correccion de errores 2

--- OAPP/OAPP/Messages.cs
+++ OAPP/OAPP/Messages.cs
@@ -73,13 +73,13 @@ namespace OAPP
             switch (value)
             {
                 case 0:
-                    msg = "{codterm:0,msg:\"OK\"}";
+                    msg = "{codterm:0,msg:\"OK\"}<EOF>";
                     break;
                 case 1:
-                    msg = "{codterm:1,msg:\"0\"}";
+                    msg = "{codterm:1,msg:\"0\"}<EOF>";
                     break;
                 case 2:
-                    msg = "{codterm:2,msg:\"Err\"}";
+                    msg = "{codterm:2,msg:\"Err\"}<EOF>";
                     break;
             }
             return msg;
